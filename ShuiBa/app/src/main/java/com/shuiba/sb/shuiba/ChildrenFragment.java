4a9ee52e253c358fea7ba92e88ad50313b2acf7d
loveguipig <1042619787@qq.com>
2015-10-28 15:40:53 +0800

    quanwenshiting

--- ShuiBa/app/src/main/java/com/shuiba/sb/shuiba/ChildrenFragment.java
+++ ShuiBa/app/src/main/java/com/shuiba/sb/shuiba/ChildrenFragment.java
@@ -4,22 +4,13 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.SequenceInputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -29,15 +20,14 @@ import java.util.List;
  */
 public class ChildrenFragment extends ListFragment{
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActivity().setTitle("故事");
-        List<Story> list = new DataProvider().getStories(getActivity().getFilesDir().getPath());
+//        List<Story> list = new DataProvider().getStories(getActivity().getFilesDir().getPath());
 
         List<String> titles = new ArrayList<String>();
-        Iterator<Story> it = list.iterator();
+        Iterator<Story> it = MainFragment.list.iterator();
         while(it.hasNext()) {
             titles.add(it.next().getName());
         }
